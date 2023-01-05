79e63db258d86719048551fecb91d48d05d1cbca
AayushMathur7 <61305658+AayushMathur7@users.noreply.github.com>
2023-01-05 12:42:45 +0800

    Update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -156,9 +156,10 @@ const MainComponent = () => {
               />
               <button className="absolute top-6 right-8 font-light font-raleway text-6xs md:text-5xs bg-white py-1 md:py-2 px-4 md:px-8 rounded-md border-2 border-black hover:-translate-y-1 hover:-translate-x-1 hover:border-b-4 hover:border-r-4 transition duration-200 text-black" onClick={() => loginWithRedirect()}>Log In</button>  
               <div className="text-[24px] md:text-[56px] mx-8 text-center font-light text-base flex-col font-black font-inter md:px-12">
-              Get personalized insights to enhance <br/> your wellbeing at work
+              Get personalized recommendations <br/> to enhance your wellbeing at work.
+              <div className="text-[16px] md:text-[24px] mt-10">Try Tranquilio’s psychologically validated 30 question assessment for free.</div>
                 <div onClick={() => setActive(1)}>
-                  <PopupButton id="vL8do7c1" className="my-button font-light font-raleway text-5xs md:text-xs bg-white py-2 md:py-4 px-8 md:px-14 rounded-md border-2 border-black hover:-translate-y-1 hover:-translate-x-1 hover:border-b-4 hover:border-r-4 transition duration-200 text-black mt-20">
+                  <PopupButton id="vL8do7c1" className="my-button font-light font-raleway text-5xs md:text-xs bg-white py-2 md:py-4 px-8 md:px-14 rounded-md border-2 border-black hover:-translate-y-1 hover:-translate-x-1 hover:border-b-4 hover:border-r-4 transition duration-200 text-black mt-16">
                     Start Assessment
                   </PopupButton>
                 </div>
