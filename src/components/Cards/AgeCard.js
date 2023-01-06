7681f98f68c272b980f209209b3c7cb77a0c4245
AayushMathur7 <61305658+AayushMathur7@users.noreply.github.com>
2023-01-06 16:02:10 +0800

    Update AgeCard.js

--- src/components/Cards/AgeCard.js
+++ src/components/Cards/AgeCard.js
@@ -18,9 +18,9 @@ const AgeCard = (props) => {
     <div className="font-inter overflow-hidden">
       <div className="flex justify-center font-medium font-ptserif text-[22px]">Your Age Group Average</div><br/>
       <br/><br/><div className="text-[72px] flex justify-center">{data[ageName]}%</div><br/><br/>
-      {data[ageName] - score > 0.5 && <div className="text-[18px] font-ptserif flex justify-center text-center">Based on your results, you have a POORER score compared to the {ageName} age group.</div>} 
-      {score - data[ageName] > 0.5 && <div className="text-[18px] font-ptserif flex justify-center text-center">Based on your results, you have a BETTER score compared to the {ageName} age group.</div>} 
-      {(data[ageName] - score <= 0.5 && score - data[ageName] <= 0.5)  && <div className="text-[18px] font-ptserif flex justify-center text-center">Based on your results, you have a SIMILAR score compared to the {ageName} age group.</div>}<br/>
+      {data[ageName] - score > 0.5 && <div className="text-[16px] md:text-[18px] font-ptserif flex justify-center text-center mx-4">Based on your results, you have a LOWER wellbeing score compared to the {ageName} age group.</div>} 
+      {score - data[ageName] > 0.5 && <div className="text-[16px] md:text-[18px] font-ptserif flex justify-center text-center mx-4">Based on your results, you have a HIGHER wellbeing score compared to the {ageName} age group.</div>} 
+      {(data[ageName] - score <= 0.5 && score - data[ageName] <= 0.5)  && <div className="text-[16px] md:text-[18px] font-ptserif flex justify-center text-center mx-4">Based on your results, you have a SIMILAR wellbeing score compared to the {ageName} age group.</div>}<br/>
       {/* {ageScore > score && <div className="text-[20px] font-ptserif flex justify-center">{Math.round(10*(ageScore-score))/10}% above your score of {score}%</div>} 
       {ageScore < score && <div className="text-[20px] font-ptserif flex justify-center">{Math.round(10*(score-ageScore))/10}% below your score of {score}%</div>} 
       {ageScore == score && <div className="text-[20px] font-ptserif flex justify-center">Equal your score of {score}%</div>} 
