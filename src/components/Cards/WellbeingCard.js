5022fb02081fd1689947a8b760068945d6649fc2
AayushMathur7 <61305658+AayushMathur7@users.noreply.github.com>
2023-01-06 16:05:33 +0800

    Update WellbeingCard.js

--- src/components/Cards/WellbeingCard.js
+++ src/components/Cards/WellbeingCard.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { PieChart, Pie, Cell, Sector, ResponsiveContainer, Tooltip, Legend, Animate } from 'recharts';
 
 const COLORS = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
-const NAMES = ["Work Demand", "Work Flexibility", "Coworker Relationship", "Management Leadership", "Compensation"];
+const NAMES = ["Work Demand", "Work Flexibility", "Coworker Relationship", "Organizational Leadership", "Compensation"];
 const CIRCLE_SIZE = 10;
 
 const RADIAN = Math.PI / 180;
@@ -35,13 +35,13 @@ const renderCustomizedLabel = ({
 
 
 const WellbeingCard = (props) => {
-  const mul = 100/(props.workDemand+props.workFlexibility+props.coworkerRelationship+props.managementLeadership+props.compensation)
+  const mul = 100/(props.workDemand+props.workFlexibility+props.coworkerRelationship+props.organizationalLeadership+props.compensation)
 
   const data = [
     { name: 'Work Demand', value: Math.round( props.workDemand*mul*10)/10 },
     { name: 'Work Flexibility', value: Math.round( props.workFlexibility*mul*10)/10 },
     { name: 'Coworker Relationship', value: Math.round( props.coworkerRelationship*mul*10)/10 },
-    { name: 'Management Leadership', value: Math.round( props.managementLeadership*mul*10)/10 },
+    { name: 'Organizational Leadership', value: Math.round( props.organizationalLeadership*mul*10)/10 },
     { name: 'Compensation', value: Math.round( props.compensation*mul*10)/10 }
   ];
 
@@ -49,8 +49,8 @@ const WellbeingCard = (props) => {
   <>
     <div className="font-inter overflow-hidden">
       <div className="flex justify-center font-medium font-ptserif text-[22px]">Wellbeing Breakdown</div><br/>
-      <div className="flex justify-center font-inter text-[18px]">
-        <PieChart width={330} height={300} delay={200}>
+      <div className="flex justify-center font-inter text-[14px]">
+        <PieChart width={300} height={250} delay={200}>
           <Pie
             data={data}
             cx="50%"
@@ -59,12 +59,15 @@ const WellbeingCard = (props) => {
             isAnimationActive={false}
             // label={renderCustomizedLabel}
             outerRadius={120}
-            fill="#8884d8"
+            fill="#ffffff"
             dataKey="value"
           >
             {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
+            <Sector
+              active={false}
+            />
           </Pie>
           <Tooltip />
           {/* <Legend iconType="circle" align="center" layout="horizontal" textStyle='black' iconSize={10.5} wrapperStyle={{fontSize:10.5}}/> */}
@@ -85,7 +88,7 @@ const WellbeingCard = (props) => {
       </div>
     ))}
     </div>
-    <div className="mt-5 mb-5 flex justify-center font-medium font-ptserif text-[13px]">Greater score indicates greater wellbeing for the constructs.</div>
+    <div className="mt-4 mb-4 flex justify-center font-medium font-ptserif text-[13px] mx-2">Comparing constructs that contribute to the highest wellbeing at work.</div>
   </div></>
 
   )
